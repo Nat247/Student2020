b6aa451cf3733b85451a372e3288d3173d3ed9d6
natalieogbeide <ogbeiden@sheridancollege.ca>
2020-01-24 11:35:55 -0500

    new branch

---
+++ src/students/PartTimeStudent.java
@@ -0,0 +1,16 @@
+
+
+package students;
+
+/**
+ *
+ * @author Nat
+ */
+public class PartTimeStudent extends Students {
+    
+       private int hoursofStudy;
+    public PartTimeStudent(String givenName, String givenID) {
+        super(givenName, givenID);
+    }
+
+}
